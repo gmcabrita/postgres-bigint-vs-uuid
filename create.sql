8afcd71b3721e5e0af00531122236ae0d9b97dfb
Gonçalo Cabrita <_@gmcabrita.com>
2023-09-20 17:44:59 +0100

    Update create.sql

--- create.sql
+++ create.sql
@@ -5,7 +5,7 @@ declare
   unix_ts_ms bytea;
   uuid_bytes bytea;
 begin
-  unix_ts_ms = substring(int8send(floor(extract(epoch from clock_timestamp()) * 1000)::bigint) from 3);
+  unix_ts_ms = substring(int8send((extract(epoch from clock_timestamp()) * 1000)::bigint) from 3);
 
   -- use random v4 uuid as starting point (which has the same variant we need)
   uuid_bytes = uuid_send(gen_random_uuid());
@@ -13,8 +13,8 @@ begin
   -- overlay timestamp
   uuid_bytes = overlay(uuid_bytes placing unix_ts_ms from 1 for 6);
 
-  -- set version 7
-  uuid_bytes = set_byte(uuid_bytes, 6, (b'0111'|| get_byte(uuid_bytes, 6)::bit(4))::bit(8)::int);
+  -- set version 7 by flipping the 2 and 1 bit in the version 4 string
+  uuid_bytes = set_bit(set_bit(uuid_bytes, 53, 1), 52, 1);
 
   return encode(uuid_bytes, 'hex')::uuid;
 end
